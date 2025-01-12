892d32fdc488659dbb16820ebc75cc38524b5abf
lddurbin <l.d.durbin@gmail.com>
2025-01-13 12:34:18 +1300

    Re-formatting

--- datacasts_film_review_data.R
+++ datacasts_film_review_data.R
@@ -14,13 +14,15 @@ library(cli)
 #' @export
 get_reviews <- function(pages = 1) {
   initiation_message(pages)
-  
+
   session <- bow("https://www.theguardian.com/film+tone/reviews", force = TRUE)
   review_links <- get_urls(session, pages)
-  
-  reviews_table <- map(review_links, ~get_review_data(session, .x)) |> 
+
+  reviews_table <- map(review_links, ~ get_review_data(session, .x)) |>
     list_rbind()
-  
+
+  check_empty_data(reviews_table, "https://www.theguardian.com/film+tone/reviews")
+
   return(reviews_table)
 }
 
@@ -33,11 +35,11 @@ get_reviews <- function(pages = 1) {
 #' @import polite rvest purrr
 #' @export
 get_urls <- function(session, pages) {
-  responses <- map(pages, ~scrape(session, query = list(page = .x)))
+  responses <- map(pages, ~ scrape(session, query = list(page = .x)))
   tags <- c("a.u-faux-block-link__overlay", "href")
-  
-  review_urls <- map(responses, ~get_attributes(.x, tags)) |> reduce(c)
-  
+
+  review_urls <- map(responses, ~ get_attributes(.x, tags)) |> reduce(c)
+
   return(review_urls)
 }
 
@@ -52,18 +54,20 @@ get_urls <- function(session, pages) {
 get_review_data <- function(session, url) {
   current_session <- nod(session, path = url)
   html_content <- scrape(current_session)
+  validate_html(html_content, url)
+  validate_element(html_content, "div.dcr-1djovmt h1", "review title")
   standfirst <- html_elements(html_content, "div.dcr-1yi1cnj")
-  
+
   if_else(
     length(html_elements(standfirst, "svg")) == 0,
     stars <- get_stars(html_content, c("figure div.dcr-15zexom svg path", "fill")),
     stars <- get_stars(standfirst, c("svg path", "fill"))
   )
-  
+
   data <- get_data_table(html_content, standfirst, url, stars) |> get_date()
-  
+
   film_review_download_status_message(data)
-  
+
   return(data)
 }
 
@@ -76,10 +80,10 @@ get_review_data <- function(session, url) {
 #' @import rvest
 #' @export
 get_text <- function(html_content, tags) {
-  text <- html_content |> 
-    html_element(tags) |> 
+  text <- html_content |>
+    html_element(tags) |>
     html_text()
-  
+
   return(text)
 }
 
@@ -92,11 +96,11 @@ get_text <- function(html_content, tags) {
 #' @import rvest
 #' @export
 get_texts <- function(html_content, tags) {
-  texts <- html_content |> 
-    html_elements(tags) |> 
-    html_text() |> 
+  texts <- html_content |>
+    html_elements(tags) |>
+    html_text() |>
     paste(collapse = "\n\n")
-  
+
   return(texts)
 }
 
@@ -104,7 +108,7 @@ get_texts <- function(html_content, tags) {
 #' Extract Attributes from HTML Elements
 #'
 #' @param html_content An HTML document object.
-#' @param tags Character vector of length 2. The first element is a CSS selector for the element, 
+#' @param tags Character vector of length 2. The first element is a CSS selector for the element,
 #' and the second is the attribute to extract.
 #' @return A character vector of extracted attribute values.
 #' @import rvest
@@ -112,11 +116,16 @@ get_texts <- function(html_content, tags) {
 get_attributes <- function(html_content, tags) {
   element_tags <- tags[[1]]
   attribute_tags <- tags[[2]]
-  
-  attributes <- html_content |> 
-    html_elements(element_tags) |> 
+
+  attributes <- html_content |>
+    html_elements(element_tags) |>
     html_attr(attribute_tags)
-  
+
+  if (is.null(attributes)) {
+    warning(glue::glue("No attributes found for tag {element_tags}."))
+    return(character(0))
+  }
+
   return(attributes)
 }
 
@@ -140,7 +149,7 @@ get_data_table <- function(html_content, standfirst, url, stars) {
     review_summary = get_text(standfirst, "p"),
     review_body = get_texts(html_content, "div#maincontent p")
   )
-  
+
   return(data_table)
 }
 
@@ -152,15 +161,15 @@ get_data_table <- function(html_content, standfirst, url, stars) {
 #' @import dplyr
 #' @export
 get_date <- function(data) {
-  data_with_date <- data |> 
+  data_with_date <- data |>
     mutate(
       year = word(review_url, 5, sep = fixed("/")),
       month = word(review_url, 6, sep = fixed("/")),
       day = word(review_url, 7, sep = fixed("/")),
       date = as.Date(paste0(year, month, day), "%Y%b%d")
-    ) |> 
+    ) |>
     select(-c(year, month, day))
-  
+
   return(data_with_date)
 }
 
@@ -175,8 +184,8 @@ get_date <- function(data) {
 #' @export
 get_stars <- function(html_content, tags) {
   stars <- get_attributes(html_content, tags)
-  stars_num <- length(stars[stars=="#121212"])
-  
+  stars_num <- length(stars[stars == "#121212"])
+
   return(stars_num)
 }
 
@@ -193,11 +202,11 @@ initiation_message <- function(pages) {
     "a page",
     paste0(length(pages), " pages")
   )
-  
+
   message <- cli_alert(
     "{.href [Politely](https://dmi3kno.github.io/polite/)} downloading {pages_num} of film reviews from {.href [The Guardian Film website](https://www.theguardian.com/uk/film)}."
   )
-  
+
   return(message)
 }
 
@@ -212,10 +221,32 @@ film_review_download_status_message <- function(data) {
   author <- data$author
   url <- data$review_url
   title <- data$film_title
-  
+
   message <- cli_alert_success(
     "Downloaded {author}'s review of {.href [{title}]({url})}."
   )
-  
+
   return(message)
+}
+
+
+validate_html <- function(html_content, url) {
+  if (is.null(html_content) || length(html_content) == 0) {
+    stop(glue::glue("Failed to load content from {url}."))
+  }
+}
+
+
+validate_element <- function(html_content, tag, element_name) {
+  element <- html_element(html_content, tag)
+  if (is.null(element)) {
+    stop(glue::glue("Missing {element_name} element for the given review."))
+  }
+}
+
+
+check_empty_data <- function(data, url) {
+  if (nrow(data) == 0) {
+    warning(glue::glue("Empty data returned for {url}. Check the structure of the HTML."))
+  }
 }
